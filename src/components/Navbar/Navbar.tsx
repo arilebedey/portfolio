42122cef325d467d3ec00c19df316ba82ba3b39a
arilebedey <34816154+arilebedey@users.noreply.github.com>
2024-07-24 11:21:53 +0200

    Organized CSS files|Reset CSS with normalize.css|Imported Valorant Font Style

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+// src/components/Navbar/Navbar.tsx
+
+import React from "react";
+import "./Navbar.css";
+
+const Navbar: React.FC = () => {
+  return <div className="nav">OK</div>;
+};
+
+export default Navbar;
