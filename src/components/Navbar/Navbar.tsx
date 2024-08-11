a904c122fb0664ba90efeb055192cbf268e6b2ef
arilebedey <34816154+arilebedey@users.noreply.github.com>
2024-08-11 13:57:38 +0200

    Fixed Navbar to top

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,12 +5,16 @@ import "./Navbar.css";
 
 const Navbar: React.FC = () => {
   return (
-    <div className="navbar">
-      <a href="project">PROJECT</a>
-      <a href="skills">SKILLS</a>
-      <a href="design">DESIGN SPECIALTY</a>
-      <a href="vision">VISION AND APPROACH</a>
-      <a href="about">ABOUT</a>
+    <div className="navbar-container">
+      <div className="navbar">
+        <div className="navbar-links">
+          <a href="project">PROJECT</a>
+          <a href="skills">SKILLS</a>
+          <a href="design">DESIGN SPECIALTY</a>
+          <a href="vision">VISION AND APPROACH</a>
+          <a href="about">ABOUT</a>
+        </div>
+      </div>
     </div>
   );
 };
